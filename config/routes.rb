8dba1316de831e47195c1ef3234eb20494f28d0f
chaitanyaratakondla <chaitanya.ratakondla@kellogg.northwestern.edu>
2022-02-20 04:24:38 +0000

    1st commit - did pretty much everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: "places#index"
+
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :places
+  resources :posts
+
 end
